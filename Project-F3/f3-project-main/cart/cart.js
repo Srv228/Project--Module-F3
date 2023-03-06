dd5e845b65af969536c8dce8a6aaf7e59466fadb
Sourav Dey <deysourav756@gmail.com>
2023-03-06 23:49:36 +0530

    first commit

---
+++ Project-F3/f3-project-main/cart/cart.js
@@ -0,0 +1,84 @@
+console.log("Running");
+let cartResult = JSON.parse(localStorage.getItem("cart"));
+let specification = JSON.parse(localStorage.getItem("specification"));
+
+console.log(specification);
+console.log(cartResult[0]);
+let parentCart = document.querySelector(".cartProducts");
+let itemTotal = 0;
+let billItem = document.querySelector(".bill");
+
+let totalArray = [];
+for (let i = 0; i < cartResult.length; i++) {
+  function getremove() {
+    let item = document.createElement("div");
+    item.className = "cartItem";
+    item.innerHTML =
+      cartResult[i] + `<button class='remove'>Remove Item</button>`;
+    parentCart.appendChild(item);
+  }
+  getremove();
+
+  let bill = document.createElement("bill");
+  bill.id = "billcontainer";
+  bill.innerHTML = ` <div class='bitem'><h3>${i + 1}.</h3><h3>${
+    specification[i].itemTitle
+  }</h3><h3>${specification[i].price}</h3></div>`;
+  billItem.appendChild(bill);
+  console.log(specification[i].price);
+  let priceitem = specification[i].price.split("");
+  priceitem.shift();
+  let total = "";
+  priceitem.forEach((element) => {
+    total = (total + element).toString();
+  });
+  total = Math.round(Number(total));
+  console.log(total);
+
+  totalArray.push(total);
+  console.log("Totalrounded: ");
+  itemTotal += total;
+  console.log(total, itemTotal, typeof total);
+  
+}
+let billsection=document.getElementById("billcontainer");
+console.log(billsection)
+let cartitembill = document.getElementsByClassName("bitem");
+let totalbill = document.createElement("div");
+totalbill.id = "totalbill";
+totalbill.innerHTML = `<h1> Total Bill: $ ${itemTotal}`;
+billItem.appendChild(totalbill);
+let removeItem = document.querySelectorAll(".remove");
+console.log(removeItem);
+let checkoutbutton = document.createElement("div");
+if (itemTotal !== 0) {
+  checkoutbutton.innerHTML = `<button id='clickCheckout'>Click to checkout</button>`;
+  totalbill.parentNode.insertBefore(checkoutbutton, totalbill.nextSibling);
+  //totalbill.insertAdjacentHTML("afterend","This is my caption.");
+  // billItem.style.marginLeft="10rem";
+}
+for (let j = 0; j < removeItem.length; j++) {
+  removeItem[j].addEventListener("click", (e) => {
+    e.target.parentElement.style.display = "none";
+    cartitembill[j].style.display = "none";
+    itemTotal = itemTotal - totalArray[j];
+    totalbill.innerHTML = `<h1> Total Bill: $ ${itemTotal} `;
+    console.log("totalbill changed");
+    if (itemTotal == 0) {
+      totalbill.innerHTML = `<h1> Hey , Its raining Offers , Add your favourite items to cart soon !</h1>`;
+      // billItem.style.marginLeft="10rem";
+      checkoutbutton.style.display = "none";
+    }
+  });
+}
+let razorpay = document.getElementById("clickCheckout");
+razorpay.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  parentCart.innerHTML =
+    "<h1 style='color:white'>Congratulations! You have successfully placed your order.<h1>";
+  parentCart.style.background='black';
+  localStorage.removeItem("cart");
+  localStorage.removeItem("specification");
+  billItem.innerHTML="<h1 style='color:white'>Go ahead and Shop again, Add your favourite items to cart soon !</h1>";
+});
---
+++ Project-F3/f3-project-main/shop/shop.js
@@ -0,0 +1,235 @@
+
+
+fetch( "https://fakestoreapi.com/products").then(res=>res.json()).then( 
+  
+res=>{
+  let menClothing=document.getElementById('mens');
+  let womenClothing=document.getElementById('womens');
+  let jeweleryStore=document.getElementById('jewelery');
+  let electronicsStore=document.getElementById('electronics');  
+  let current;
+  let buttons=document.querySelectorAll('.filter');  
+  
+  let mens ,womens,jewelery,electronics;
+  mens=res.filter( (product)=>product.category==="men's clothing")
+  console.log(mens)
+   womens=res.filter( (product)=>product.category==="women's clothing")
+  console.log(womens)
+   jewelery=res.filter( (product)=>product.category==="jewelery");
+  console.log(jewelery)
+  electronics=res.filter( (product)=>product.category==="electronics");
+  console.log(electronics)       
+
+  localStorage.setItem('response',JSON.stringify(res))
+  function getbuttons(){
+
+    mens.map( (item)=>{
+   
+      menClothing.innerHTML+= `<div class="item">
+      <div class='siblingcart'>
+      <img src=${item.image} alt="Item" />
+      <div class="info">
+      <h5>${item.title}</h5>
+        <div class="row">
+          <div class="price">$${item.price}</div>
+          <div class="sized">S,M,L</div>
+        </div>
+        <div class="colors">
+          Colors:
+          <div class="row">
+            <div class="circle" style="background-color: #000"></div>
+            <div class="circle" style="background-color: #4938af"></div>
+            <div class="circle" style="background-color: #203d3e"></div>
+          </div>
+        </div>
+        <div class="row">Rating:${item.rating.rate}</div>
+      </div></div>
+      <button class="addBtn" >Add to Cart</button>
+      </div>`    
+     
+     
+      
+    }
+    )
+    
+    
+    
+    womens.map( (item)=>{
+    womenClothing.innerHTML+= `<div class="item">
+    <div class='siblingcart'>
+     <img src=${item.image} alt="Item" />
+     <div class="info">
+       <h5>${item.title}</h5>
+       <div class="row">
+        <div class="price">$${item.price}</div>
+        <div class="sized">S,M,L</div>
+       </div>
+       <div class="colors">
+       Colors:
+       <div class="row">
+        <div class="circle" style="background-color: #000"></div>
+        <div class="circle" style="background-color: #4938af"></div>
+        <div class="circle" style="background-color: #203d3e"></div>
+      </div>
+      </div>
+    <div class="row">Rating:${item.rating.rate}</div>
+    </div>
+    </div>
+    <button class="addBtn" >Add to Cart</button>
+    </div>`  
+  
+    
+  
+    }
+    
+    )
+    
+    jewelery.map( (item)=>{
+    jeweleryStore.innerHTML+= `<div class="item">
+    <div class='siblingcart'>
+    <img src=${item.image} alt="Item" />
+    <div class="info">
+    <h5>${item.title}</h5>
+    <div class="row">
+      <div class="price">$${item.price}</div>
+      <div class="sized">S,M,L</div>
+    </div>
+    <div class="colors">
+      Colors:
+      <div class="row">
+        <div class="circle" style="background-color: #000"></div>
+        <div class="circle" style="background-color: #4938af"></div>
+        <div class="circle" style="background-color: #203d3e"></div>
+      </div>
+    </div>
+    <div class="row">Rating:${item.rating.rate}</div>
+    </div>
+    </div>
+    <button class="addBtn">Add to Cart</button>
+    </div>`  
+    }
+    ) 
+    
+    electronics.map( (item)=>{
+    electronicsStore.innerHTML+= `<div class="item">
+    <div class='siblingcart'>
+   
+    <img src=${item.image} alt="Item" />
+    <div class="info">
+    <h5>${item.title}</h5>
+    <div class="row">
+      <div class="price">$${item.price}</div>
+      <div class="sized">S,M,L</div>
+    </div>
+    <div class="colors">
+      Colors:
+      <div class="row">
+        <div class="circle" style="background-color: #000"></div>
+        <div class="circle" style="background-color: #4938af"></div>
+        <div class="circle" style="background-color: #203d3e"></div>
+      </div>
+    </div>
+    <div class="row">Rating:${item.rating.rate}</div>
+    </div>
+    </div>
+    <button class="addBtn">Add to Cart</button>
+    </div>`  
+    }
+    )
+  }
+  getbuttons();
+ let cart=document.querySelectorAll('.addBtn')
+ 
+   let cartitems=[];
+   let productinfo=[];
+
+    console.log(cart);
+    let itemTitle , price;
+    for(let i=0; i<cart.length; i++){
+      console.log('addtocart')
+      cart[i].addEventListener('click',(e)=>{
+       itemTitle=e.target.previousElementSibling.firstElementChild.nextElementSibling.firstElementChild.innerText;
+        price=e.target.previousElementSibling.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild.innerText;
+        productinfo.push({'itemTitle':itemTitle, 'price':price})
+          cart[i].style.backgroundColor='green'; 
+          cart[i].innerText='Added to cart'; 
+          localStorage.setItem('specification', JSON.stringify(productinfo))
+        console.log(productinfo)
+        cartitems.push(e.target.previousElementSibling.innerHTML);
+       // console.log(cartitems)
+        localStorage.setItem('cart', JSON.stringify(cartitems))
+      })
+   
+    }
+   // let addtocart=document.querySelectorAll('.addBtn');
+    
+  
+
+
+const btns=document.querySelectorAll('.btns');
+const products=document.querySelectorAll('.items');
+
+for(let i=0;i<btns.length;i++) {
+btns[i].addEventListener('click',(e)=>{
+    e.preventDefault();
+    const filter=e.target.dataset.filter;
+    console.log(filter);
+    let current=document.querySelector('.active')
+  
+  if(
+    !btns[i].classList.contains('active')){
+      btns[i].classList.add('active');
+      current.classList.remove('active');
+    }
+    products.forEach(product=>{
+if(filter=='all'){
+    product.style.display='flex';
+    product.style.flexdirection ='column';
+}
+else{
+    if(product.classList.contains(filter)){
+        product.style.display='flex';
+    }
+    else{
+        product.style.display='none';
+
+    }
+}
+
+    })
+})  
+
+
+
+}
+
+
+const search=document.getElementById('search');
+
+search.addEventListener("keyup",(e)=>{
+    e.preventDefault();        
+    const searchValue=search.value;
+    console.log(searchValue);
+//         res.forEach(data=>{
+// if(data.title.includes(search.value)){
+// console.log(data.title);
+// }
+     for(i=0;i<products.length;i++){
+       if(products[i].classList.contains(searchValue)){
+    console.log("insideif")
+        products[i].style.display='flex';
+           }
+           else if(searchValue===''){
+            products[0].style.display='flex';
+           }
+         else{                 
+        products[i].style.display='none';
+    
+         }
+       }
+    })
+    
+})
+
+
+
